200eaeb5953e6ac0b5d36a7842137dd2efb81165
ZadorozhniiVova <zadorozhniiv@gmail.com>
2022-08-16 23:30:25 +0300

    update

--- Lesson34/assets/js/main.js
+++ Lesson34/assets/js/main.js
@@ -1,69 +1,169 @@
-// allCharasters = fetch('https://rickandmortyapi.com/api/character')
-
-// console.log(allCharasters)
-// allCharasters.then((response) => {
-//     // console.log('response', response.json())
-//     response.json().then((data) => {
-//         console.log('data', data)
-//     })
-// })
-
-const URL = 'https://reqres.in/api'
-async function getUsers(){
-
-    const response = await fetch(`${URL}/users`, {mode:'cors'})
-    const respJSON = await response.json()
-    return respJSON.data
-           
-}
-getUsers().then((users) => {
-    console.log('users', users)
-
-})
-
-async function removeUser(id){
-    const response = await fetch(`${URL}/users/${id}`, {method: 'DELETE'})
-    console.log(response.status)
-    return response.status
-}
-
-
 $(function(){
-    const container = $('#demo')
-    getUsers().then((users) => {
-      console.log('users', users)
-      users.forEach(user => {
-        const card = $(`<div class="card" id='${user.id}'></div>`)
-        const image = $(`<img class="avatar" src='${user.avatar}'>`)
-        console.log('image', image)
-        const info =$(`<div class="info"></div>`)
-
-        info.append($(`<div class="fullname">name: ${user.first_name} ${user.last_name}</div>`))
-        info.append($(`<div class="email">email: ${user.email}</div>`))
-        info.append($(`<div class="id">id: ${user.id}</div>`))
-        info.append($(`<button class="update" onclick="updateUser (${user.id})">Update</button>`))
-        info.append($(`<button class="delete" onclick="deleteUser (${user.id})">Delete</button>`))
-
-        card.append(image)
-        card.append(info)
-        container.append(card)
-      })
-    })
-})
-
-const updateUser = function(id){
-    alert(id);
-}
-
-const deleteUser = function(id){
-    // alert('delete user'+id);
-    removeUser(id).then( status => {
-        if(status === 204){
-            $(`#${id}`).remove()
-        }
-    })
 
     
-}
-  
+
+    const filmDB='http://omdbapi.com';
+    const i = '?i=tt3896198';
+    const apiKey='apikey=7c130cb8';
+    $('#searchButton').on('click', function(){
+        let pageNum = 1;
+        let movieName = $('#search').val();
+        let movieType = $('#type').val(); 
+        const filmTop = $('#filmRight')
+
+        filmTop.html('')
+        filmTop.css('border', 'none')
+        
+
+        async function getSearch(){
+            
+
+            if(movieName !== '' && movieType !== 'undefined'){
+                filmTop.html('')
+                filmTop.css('border', 'none')
+                
+                const responce = await fetch(`${filmDB}/?s=${movieName}&type=${movieType}&${i}&${apiKey}&page=${pageNum}`)
+                const respJson = await responce.json()
+
+                if(parseInt(respJson.totalResults)>0){
+                    const respJsonSearch = respJson.Search;
+                    console.log(respJsonSearch)
+
+                    for(let f = 0; f <=respJsonSearch.length; f++){
+                        const filmItem = $(`<div class="film__item" id="${respJsonSearch[f].imdbID}"></div>`)
+                        filmItem.css('border', '2px solid brown')
+                        filmItem.append($(`<h1 class="film__title">Film Name: ${respJsonSearch[f].Title}</h1>`));
+                        
+                        filmItem.append($(`<div class="film__add"><span class="film__add-el">Year:</span><span class="film__info"> ${respJsonSearch[f].Year}</span></div>`));
+                        filmItem.append($(`<div class="film__add"><span class="film__add-el">Type:</span><span class="film__info"> ${respJsonSearch[f].Type}</span></div>`));                    
+                        if(respJsonSearch[f].Poster !== 'N/A'){
+                            filmItem.append($(`<img class='film__poster' src="${respJsonSearch[f].Poster}">`));
+                        }else{
+                            filmItem.append($(`<img class='film__poster' src="https://cdn2.wpbeginner.com/wp-content/uploads/2013/04/wp404error.jpg">`));
+                        }
+                        filmTop.append(filmItem);
+                        
+                        
+                    }
+                }else{
+                    alert(respJson.Error)
+                }
+                
+                
+            } else{
+                alert('Write Name and choose Type')
+            } 
+            
+                 
+        }
+        
+        
+        
+        
+        getSearch();
+        
+        async function pageSearch(){
+            $("#pagination").html('')
+            const totalFilms = await fetch(`${filmDB}/?s=${movieName}&type=${movieType}&${apiKey}`);
+            const totalFilmsJson = await totalFilms.json();
+            const totalPages = parseInt(totalFilmsJson.totalResults / 10)
+    
+            for(let page=1;page<=totalPages;page++){
+                $("#pagination").append($(`<li class="filmPage">${page}</li>`)); 
+            }
+
+            let items = $('.filmPage');
+        
+            items.on('click', function(event){
+                pageNum = parseInt(event.target.innerText)                    
+                getSearch();
+
+            })
+             
+        }
+        pageSearch();
+
+        function showInfo(){
+                
+            $('#filmRight').on('click', async function(event){
+                
+                const InfoURL = "http://www.omdbapi.com/"
+                let targetId = event.target.parentNode.getAttribute("id")
+                if(targetId !== 'null' && targetId !== 'filmRight' && targetId.Response !== 'False'){
+                    $('.modal').css("opacity", "0");
+                    $('.modal').css("z-index", "-1");
+                    $('.modal').html('');
+                    console.log(targetId)
+                    const filmInfo = await fetch(`${InfoURL}?i=${targetId}&${apiKey}`)
+                    const filmInfoJson = await filmInfo.json();
+                    console.log(filmInfoJson)
+
+                    $('.modal').append($(`<div class="modal__title"><h2 class="modal__title-text">Film Info</h2></h2></div>`));
+                    $('.modal').append($(`<svg class="modal__button" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 378.303 378.303" style="enable-background:new 0 0 378.303 378.303;"xml:space="preserve"><polygon style="fill:#FF3501;" points="378.303,28.285 350.018,0 189.151,160.867 28.285,0 0,28.285 160.867,189.151 0,350.018 28.285,378.302 189.151,217.436 350.018,378.302 378.303,350.018 217.436,189.151 "/></svg>
+                    `));
+                    $('.modal').append($(`<div class="modal__info"></div>`));
+
+                    if(filmInfoJson.Poster !== 'N/A'){
+                        $('.modal__info').append($(`<div class="modal__left"><img class="modal__poster" src="${filmInfoJson.Poster}"></div>`));
+                    }else{
+                        $('.modal__info').append($(`<div class="modal__left"><img class="modal__poster" src="https://cdn2.wpbeginner.com/wp-content/uploads/2013/04/wp404error.jpg"></div>`));
+                    }
+                    $('.modal__info').append($(`<div class="modal__right"></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Actors:</span><span class="modal__el-info">${filmInfoJson.Actors}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Awards:</span><span class="modal__el-info">${filmInfoJson.Awards}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">BoxOffice:</span><span class="modal__el-info">${filmInfoJson.BoxOffice}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Country:</span><span class="modal__el-info">${filmInfoJson.Country}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">DVD:</span><span class="modal__el-info">${filmInfoJson.DVD}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Director:</span><span class="modal__el-info">${filmInfoJson.Director}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Genre:</span><span class="modal__el-info">${filmInfoJson.Genre}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Language:</span><span class="modal__el-info">${filmInfoJson.Language}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Metascore:</span><span class="modal__el-info">${filmInfoJson.Metascore}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Plot:</span><span class="modal__el-info">${filmInfoJson.Plot}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Ratings:</span><span class="modal__el-info">${filmInfoJson.imdbRating}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Released:</span><span class="modal__el-info">${filmInfoJson.Released}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Runtime:</span><span class="modal__el-info">${filmInfoJson.Runtime}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Writer:</span><span class="modal__el-info">${filmInfoJson.Writer}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Year:</span><span class="modal__el-info">${filmInfoJson.Year}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Actors:</span><span class="modal__el-info">${filmInfoJson.Actors}</span></div>`));
+                    $('.modal__right').append($(`<div class="modal__el"><span class="modal__el-name">Actors:</span><span class="modal__el-info">${filmInfoJson.Actors}</span></div>`));
+                    $('.modal').css("opacity", "1");
+                    $('.modal').css("z-index", "3");
+                    
+                }
+
+                function closeInfo(){
+                    $('.modal__button').on('click', () => {
+                        console.log('click')
+                        
+                        $('.modal').css("opacity", "0");
+                        $('.modal').css("z-index", "-1");
+                    })
+                }
+                closeInfo();
+                
+            })
+        }
+        showInfo() 
+
+        
+
+        
+    })
+
+   
+
+    
+    
+})
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ Lesson35/assets/js/main.js
@@ -0,0 +1,228 @@
+// Initialize and add the map
+function initMap() {
+
+
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: uluru,
+    styles: [{
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#1d2c4d"
+        }]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#8ec3b9"
+        }]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+          "color": "#1a3646"
+        }]
+      },
+      {
+        "featureType": "administrative.country",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+          "color": "#4b6878"
+        }]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#64779e"
+        }]
+      },
+      {
+        "featureType": "administrative.province",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+          "color": "#4b6878"
+        }]
+      },
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+          "color": "#334e87"
+        }]
+      },
+      {
+        "featureType": "landscape.natural",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#023e58"
+        }]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#283d6a"
+        }]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#6f9ba5"
+        }]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+          "color": "#1d2c4d"
+        }]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "color": "#023e58"
+        }]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#3C7680"
+        }]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#304a7d"
+        }]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#98a5be"
+        }]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+          "color": "#1d2c4d"
+        }]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#2c6675"
+        }]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+          "color": "#255763"
+        }]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#b0d5ce"
+        }]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+          "color": "#023e58"
+        }]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#98a5be"
+        }]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+          "color": "#1d2c4d"
+        }]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "color": "#283d6a"
+        }]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#3a4762"
+        }]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#0e1626"
+        }]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#4e6d70"
+        }]
+      }
+    ]
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: {
+      lat: 50.37,
+      lng: 30.48
+    },
+    title: "somewhere",
+    map,
+  });
+  const infowindow = new google.maps.InfoWindow({
+    content: "<div>Тут сегодня отшрафовали студента</div>",
+  });
+
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+      shouldFocus: false,
+    });
+  });
+
+
+  const markers = []
+  for (let i = 0; i < 5; i++) {
+    const marker = new google.maps.Marker({
+      position: {
+        lat: 50.37 + (i * 0.05),
+        lng: 30.48 + (i * 0.05)
+      },
+      title: `Долго ${i+1}00 грн`,
+      map,
+    });
+  }
+
+
+}
+
+
+window.initMap = initMap;
